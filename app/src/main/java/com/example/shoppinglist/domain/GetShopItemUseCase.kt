e6ffcba72b0986dfca6f13457d5acc1c840d6ed4
kpm32 <kpm32@mail.ru>
2023-01-11 17:42:17 +0600

    Add data class  ShopItem Add four class

---
+++ app/src/main/java/com/example/shoppinglist/domain/GetShopItemUseCase.kt
@@ -0,0 +1,9 @@
+package com.example.shoppinglist.domain
+
+class GetShopItemUseCase {
+    // метод для редактирования, принимает id целого типа
+    // и возвращает наш класс
+    fun getShopItem(shopItemId: Int): ShopItem{
+        TODO()
+    }
+}
